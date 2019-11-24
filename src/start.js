610de3dd708650cfcbf68334e8dcc5f5ebf56e73
psybient <nima.psybient@gmail.com>
2019-11-24 12:48:17 +0330

    Add(electron-is-dev) : Linkk web and electron

--- src/start.js
+++ src/start.js
@@ -1,15 +1,25 @@
 const electron = require('electron')
 const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-
 const path = require('path')
+const isDev = require('electron-is-dev')
+const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
 
-  mainWindow.loadURL(`file://${path.join(__dirname, '../public/index.html')}`)
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  )
 
   mainWindow.on('closed', () => {
     mainWindow = null
